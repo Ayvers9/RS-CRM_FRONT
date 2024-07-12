90ff5b8b496297ed6b8294f214129e9f381f61ad
1DamnDaniel3 <100968388+1DamnDaniel3@users.noreply.github.com>
2024-07-12 19:19:55 +0300

    Changed photoForm for profilePage added func ti load images

--- src/components/common/photoForm.js
+++ src/components/common/photoForm.js
@@ -1,18 +1,45 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import api from "../../services/api";
+import placeholderImage from "../../assets/images/img-placeholder.png";
 
 const PhotoUpload = () => {
+    //photo & preview
     const [selectFile, setSelectFile] = useState(null);
     const [preview, setPreview] = useState(null);
+    // complete upload
+    const [isUploadBtnVisible, setUploadBtnVisible] = useState(false)
+    const [isLoading, setLoading] = useState(false)
+    const [successMessage, setSuccessMessage] = useState(false)
+    const fileInputRef = useRef(null)
 
+    // load image from localStorage
+    useEffect(()=>{
+        const savedImage = localStorage.getItem("uploadedImage");
+        // localStorage.clear() // to clear all images from localStorage
+        if(savedImage){
+            setPreview(savedImage);
+        }
+        else{
+            setPreview(placeholderImage)
+        }
+    }, []);
+
+    // For update state of image form
     const handleFileChange = (event) => {
         const file= event.target.files[0];
-        setSelectFile(file);
-        setPreview(URL.createObjectURL(file));
+        if (file){
+            const previewURL = URL.createObjectURL(file);
+            setSelectFile(file);
+            setPreview(previewURL);
+            localStorage.setItem("uploadedImage", previewURL);
+            setUploadBtnVisible(true)
+        }
     };
 
+    // upload confirm
     const handleSubmit = async(event) => {
         event.preventDefault();
+        setLoading(true)
         const formData = new FormData();
         formData.append('photo', selectFile);
 
@@ -23,17 +50,56 @@ const PhotoUpload = () => {
                 },
             });
             console.log('File uploaded successfully', response.data);
+            // Set loading to false and show success message after a delay
+            setTimeout(() => {
+                setLoading(false);
+                setSuccessMessage(true);
+                
+
+                // Hide success message after a delay
+                setTimeout(() => {
+                    setSuccessMessage(false);
+                }, 2000);
+            }, 2000); // Delay to simulate loading time
+            setUploadBtnVisible(false);
         }catch(error){
             console.error('Error uploading file:', error)
+            setLoading(false);
         }
     };
-
-    return(
-        <div>
+    
+    // draw form
+    return (
+        <div className="photoFormContainer">
             <form className="photoForm" onSubmit={handleSubmit}>
-                <input className="photoFormInput" type="file" onChange={handleFileChange}/>
-                {preview && <img src={preview} alt="Preview" width="100"/>}
-                <button className="photoFormBtn" type="submit">Upload photo</button>
+                <input
+                    className="photoFormInput"
+                    type="file"
+                    onChange={handleFileChange}
+                    style={{ display: "none" }} // Скрыть input
+                    ref={fileInputRef}
+                />
+                <label className="photoInput" htmlFor="fileInput" onClick={() => fileInputRef.current.click()}>
+                    <div className="imageContainer">
+                        {isLoading ? (
+                            <h2 className="loading">Loading...</h2>
+                        ) : (
+                            <img
+                                src={preview || "/images/img-placeholder.png"}
+                                alt="Preview"
+                                className="photoPreview"
+                            />
+                        )}
+                    </div>
+                </label>
+                <div className="messages">
+                    {isUploadBtnVisible && (
+                        <button className="photoFormBtn" type="submit">Upload photo</button>
+                    )}
+                    {successMessage && (
+                        <h2 className="successfulMessage">Successful!</h2>
+                    )}
+                </div>
             </form>
         </div>
     );
--- src/components/pages/profilePage.js
+++ src/components/pages/profilePage.js
@@ -2,44 +2,25 @@ import React, { useState, useEffect } from 'react';
 import {jwtDecode} from 'jwt-decode';
 import api from '../../services/api';
 import "./styles/profileStyle.css"
+import "../../assets/styles/photoForm.css"
 import PhotoForm from '../common/photoForm'
 
 const Profile = () => {
     const [userData, setUserData] = useState(null);
     const [errorMessage, setErrorMessage] = useState('');
     
-    // useEffect(() => {
-    //     const initializeUser = async () => {
-    //         try {
-
-    //         } catch (error) {
-    //             console.error('Ошибка при инициализации пользователя:', error);
-    //             setErrorMessage('Не удалось получить данные пользователя');
-    //         }
-    //     };
-
-    //     initializeUser();
-    // }, []);
-
-    // if (errorMessage) {
-    //     return <div className='error-message'>{errorMessage}</div>;
-    // }
-
-    // if (!userData) {
-    //     return <div>Загрузка...</div>;
-    // }
 
     return(
         <div className='globalspace'>
             <div className="mainSpace">
                 <div className="leftPart">
-                    <div>
+                    <div className='leftPartContainer'>
                         <PhotoForm/>
                     </div>
 
                 </div>
                 <div className="rightPart">
-                    <div>
+                    <div className='rightPartContainer'>
                             DSADASDASDASDS
                     </div>
                 </div>
